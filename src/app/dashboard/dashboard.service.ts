ea1e34496c4d9c9492536c83ec4cf286d1223daf
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-03 21:19:16 -0300

    creating service

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Stock from '../shared/models/stock-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  readonly baseUrl = 'http://quotation-system-santander.herokuapp.com/quotation'
+
+  constructor(private http: HttpClient) { }
+
+  async getStocks(): Promise<Stock[]> {
+    return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
+  }
+}
