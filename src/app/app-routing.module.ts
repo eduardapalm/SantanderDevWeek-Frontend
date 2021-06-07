dd43156d6332a137e3b6a21c251ae4223431ac92
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-07 12:54:08 -0300

    final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardPageComponent }
+  { path: '/', component: DashboardPageComponent }
 ];
 
 @NgModule({
