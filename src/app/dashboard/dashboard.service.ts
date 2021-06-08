52b52c9e9996a327be5cf329c3127d6e0529f069
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-07 22:27:54 -0300

    fix/fixing API url

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://quotation-system-santander.herokuapp.com/quotation'
+  readonly baseUrl = 'https://quotation-system-santander.herokuapp.com/quotation'
 
   constructor(private http: HttpClient) { }
 
